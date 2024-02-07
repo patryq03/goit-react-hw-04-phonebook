14a82e83f5c0dca1b28c252f022e3e2d0d0d8bea
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-02-07 18:53:13 +0100

    feat: change src folder

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,36 +1,29 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
 
-class Contacts extends Component {
-  deleteId = Id => {
-    this.props.del(Id);
-  };
-  createList = () => {
-    return this.props.contacts.map(contact => {
-      return (
-        <li key={contact.id} id={contact.id}>
-          {`${contact.name}: ${contact.number}`}
-          <button
-            data-id={contact.id}
-            onClick={() => this.deleteId(contact.id)}
-          >
-            Delete
-          </button>
-        </li>
-      );
-    });
-  };
+const Contacts = ({ contacts, onRemoveContact }) => {
 
-  render() {
-    return <ul>{this.createList()}</ul>;
-  }
-}
+  return(
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id} id={contact.id}>
+          {contact.name + ' : ' + contact.number}
+          {
+            <button
+              type="button"
+              name="delete"
+              onClick={() => onRemoveContact(contact.id)}>
+              Delete
+            </button>
+          }
+        </li>
+      ))}
+    </ul>)
+    };
+
 Contacts.propTypes = {
   contacts: PropTypes.array.isRequired,
-  del: PropTypes.func.isRequired,
-};
-Contacts.defaultProps = {
-  contacts: [],
+  onRemoveContact: PropTypes.func.isRequired,
 };
 
 export default Contacts;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,64 +1,69 @@
 import css from './Form.module.css';
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Form extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+const Form = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    let contactForAdd = { name: this.state.name, number: this.state.number };
-    this.props.onSubmitData(contactForAdd);
-
-    this.reset();
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-  render() {
-    return (
-      <div className={css.form}>
-        <form type="submit" onSubmit={this.handleSubmit}>
-          <label>
-            <p className={css.p}>Name</p>
-            <input
-              type="text"
-              name="name"
-              placeholder='Contact name'
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              onChange={this.handleChange}
-              value={this.state.name}
-            />
-          </label>
-          <label>
-            <p className={css.p}>Number</p>
-            <input
-              type="tel"
-              name="number"
-              placeholder='XXX-XX-XX'
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              onChange={this.handleChange}
-              value={this.state.number}
-            />
-          </label>
-          <button type="submit" className={css.button}>Add contact</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.form}>
+      <form type="submit" onSubmit={handleSubmit}>
+        <label>
+          <p className={css.p}>Name</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="Contact name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={handleChange}
+            value={name}
+          />
+        </label>
+        <label>
+          <p className={css.p}>Number</p>
+          <input
+            type="tel"
+            name="number"
+            placeholder="XXX-XX-XX"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={handleChange}
+            value={number}
+          />
+        </label>
+        <button type="submit" className={css.button}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
 
 Form.propTypes = {
   onSubmitData: PropTypes.func.isRequired,
